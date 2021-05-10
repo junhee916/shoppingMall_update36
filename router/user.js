c3953e683923199dd7c25a83a779b84aefae33f9
junhee916 <junhee916@naver.com>
2021-05-10 12:30:30 +0900

    setting restApi/mongodb

---
+++ router/user.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const {
+    users_get_all,
+    users_signup_user,
+    users_login_user,
+    users_delete_all
+} = require('../contorller/user')
+const router = express.Router()
+
+// get userInfo
+router.get('/', users_get_all)
+
+// sign up
+router.post('/signup', users_signup_user)
+
+// login
+router.post('/login', users_login_user)
+
+// delete userInfo
+router.delete('/', users_delete_all)
+
+module.exports = router
